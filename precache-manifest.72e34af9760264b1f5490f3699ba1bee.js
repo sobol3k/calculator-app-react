1be46bcc51f8a5ce0fcb8fc4f0cb6733f8cc287a
Sobol3k <sobolek66@gmail.com>
2019-09-01 23:59:22 +0200

    Updates

---
+++ precache-manifest.72e34af9760264b1f5490f3699ba1bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a191fabd1dc71f1dac08",
+    "url": "/calculator-app-react/static/js/runtime~main.9a6707e0.js"
+  },
+  {
+    "revision": "d6ef8bb68338c22817dd",
+    "url": "/calculator-app-react/static/js/main.ab884bb0.chunk.js"
+  },
+  {
+    "revision": "e56839eb30a7ff470b16",
+    "url": "/calculator-app-react/static/js/2.a0cf9c8e.chunk.js"
+  },
+  {
+    "revision": "d6ef8bb68338c22817dd",
+    "url": "/calculator-app-react/static/css/main.08d7ff65.chunk.css"
+  },
+  {
+    "revision": "37c18cef91ef2456963ee1a350f56e8c",
+    "url": "/calculator-app-react/index.html"
+  }
+];
